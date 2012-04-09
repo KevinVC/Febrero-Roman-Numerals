e44fe3407f4e09489dbc69c04edd88a59c195fac
Kevin Vega <kevinvcm@gmail.com>
2012-04-09 17:22:33 -0500

    Numeros del 1 al 10

--- kevinVC/romannumerals.rb
+++ kevinVC/romannumerals.rb
@@ -1,27 +1,21 @@
 class RomanNumerals
 
+	SYMBOLS = [["X",10], ["IX",9], ["V",5], ["IV",4],["I",1]]
+
 	def convert_Roman(num)
 		if num == 0
-			raise "El 0 no se puede convertir"
+			raise "el 0 no se puede convertir"
 		end
 
-		if num == 4
-			return "IV"
-		end
-		if num == 5
-			return "V"
-		end
+	    result = ""
+	    SYMBOLS.each do | romano, numero |
+	      while num >=  numero
+	        result += romano
+	        num -= numero
+	      end
+	    end
 
-		result = ""
-
-		while num >= 1 && num <= 3
-			result += "I"
-			num -= 1
-
-		end
-		return result
-
-
+	    return result
 	end
 
 end
--- kevinVC/romannumerals_spec.rb
+++ kevinVC/romannumerals_spec.rb
@@ -12,27 +12,42 @@ describe RomanNumerals do
 		}.should raise_exception
 	end
 
-	context "convertir a romanos" do
-		it "convertir 1 en I" do
+	context "convertir simbolos (I,V,X,L,C,D,M)" do
+	    it "convertir 1 en I" do
 			@romano.convert_Roman(1).should == "I"
+	    end
+
+	    it "convertir 5 en V" do
+			@romano.convert_Roman(5).should == "V"
+	    end
+
+		it "convertir 10 en X" do
+			@romano.convert_Roman(10).should == "X"
 		end
 
-		it "convertir 2 en II" do
-			@romano.convert_Roman(2).should == "II"
+	end
+
+	context "Aumentar un valor " do
+	    it "convertir 2 en II" do
+	     @romano.convert_Roman(2).should == "II"
 		end
 
 		it "convertir 3 en III" do
 			@romano.convert_Roman(3).should == "III"
 		end
 
+		it "convertir 8 en VIII" do
+			@romano.convert_Roman(8).should == "VIII"
+		end 
+	end
+
+	context "Disminuir un valor " do
 		it "convertir 4 en IV" do
 			@romano.convert_Roman(4).should == "IV"
 		end
-
-		it "convertir 5 en V" do
-			@romano.convert_Roman(5).should == "V"
+		it "convertir 9 en IX" do
+			@romano.convert_Roman(9).should == "IX"
 		end
-
 	end
 	
 end
